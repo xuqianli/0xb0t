65b4ffade929397927822f9d11c5ae3cebba2326
Steven Li <xq6li@uwaterloo.ca>
2015-02-28 21:24:22 -0500

    test commit

--- app.js
+++ app.js
@@ -1,34 +1,63 @@
+//------------------------------------------------------------------------------
+//                         Import Node Modules
+//------------------------------------------------------------------------------
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var socket = require("socket.io");
 var SerialPort = require("serialport").SerialPort
 
+//------------------------------------------------------------------------------
+//                        Serial Port Connection
+//------------------------------------------------------------------------------
+var receivedData = "";
+var serialPort = new SerialPort("/dev/tty.usbmodem1451", {
 
-var serialPort = new SerialPort("/dev/ttyACM0", {
+  // baudrate is synced to Arduino
   baudrate: 9600,
+
+  // Default settings for Arduino serial connection
   dataBits: 8,
   parity: 'none',
   stopBits: 1,
   flowControl: false,
+
 }, false); // this is the openImmediately flag [default is true]
 
+// Open new event
 serialPort.open(function (error) {
   if ( error ) {
     console.log('failed to open: '+error);
   } else {
     console.log('open');
+
+    // Listens to incoming data
     serialPort.on('data', function(data) {
-      console.log('data received: ' + data);
+
+      receivedData += data;
+      console.log('data received: ' + receivedData);
+
+      if (receivedData.indexOf('#') >= 0 && receivedData.indexOf('$') >= 0) {
+        // save the data between '$' and '#'
+        sendData = receivedData.substring(receivedData.indexOf('$') + 1,
+        receivedData.indexOf('#'));
+        console.log('data received: ' + sendData);
+        receivedData = '';
+	     }
+
     });
+
     serialPort.write("ls\n", function(err, results) {
       console.log('err ' + err);
       console.log('results ' + results);
     });
   }
 });
+
+
 
 
 
---
+++ slave/slave.ino
@@ -0,0 +1,151 @@
+//Sonar 1 ---------- left
+int echoPin1 =9;
+int initPin1 =10;
+int distance1 =0;
+
+//Sonar 2 ----------- middle
+int echoPin2 =4;
+int initPin2 =5;
+int distance2 =0;
+
+//Sonar 3 ----------- right
+int echoPin3 =2;
+int initPin3 =3;
+int distance3 =0;
+
+//defining constants
+const int speedA = 200;
+const int speedB = 200;
+
+int _direction = 0; // 0 is forward
+//
+
+void setup() {
+  //--------Setup Channel A--------//
+
+  //Initiates Motor Channel A pins
+  pinMode (13, OUTPUT);
+  pinMode (12, OUTPUT);
+  pinMode (11, OUTPUT);
+
+
+  //--------Setup Channel B--------//
+
+  //Initiates Motor Channel B pin
+  pinMode (8, OUTPUT);
+  pinMode (7, OUTPUT);
+  pinMode (6, OUTPUT);
+
+  
+  
+  pinMode(initPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(initPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(initPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+
+  delay (10);
+  //taking distances from sonars
+  distance1 = getDistance(initPin1, echoPin1);
+  delay (50);
+  distance2 = getDistance(initPin2, echoPin2);
+  delay (50);
+  distance3 = getDistance(initPin3, echoPin3);
+  delay (50);
+   //initializing speed
+  analogWrite (11, speedA);
+ // digitalWrite (13, LOW);
+  analogWrite (6, speedB);
+  
+  Serial.print ("#1: ");
+  Serial.println (distance1);
+  Serial.print ("#2: ");
+  Serial.println (distance2);
+  Serial.print ("#3: ");
+  Serial.println (distance3);
+
+}
+
+
+// gets the distance of the sonar
+int getDistance (int initPin, int echoPin){
+
+ digitalWrite(initPin, HIGH);
+ delayMicroseconds(2); 
+ digitalWrite(initPin, LOW); 
+ 
+ unsigned long pulseTime = pulseIn(echoPin, HIGH); 
+ int distance = pulseTime/58;
+ return distance;
+ 
+}
+ 
+ 
+ 
+void  forward (int time) {
+   
+  analogWrite (11, speedA);   
+  analogWrite (6, speedB); 
+  // channel B  
+  digitalWrite (8, LOW);
+  digitalWrite (7, HIGH); 
+  
+   // channel A
+  digitalWrite (13, LOW);
+  digitalWrite (12, HIGH);
+  delay (time);
+}
+
+void backward (int time) {
+  
+  analogWrite (11, speedA);   
+  analogWrite (6, speedB); 
+  // channel B   
+  digitalWrite (8, HIGH);
+  digitalWrite (7, LOW); 
+  
+   // channel A
+  digitalWrite (13, HIGH);
+  digitalWrite (12, LOW);
+  delay (time);
+}
+
+void left (int time) {
+  analogWrite (6, 150);
+  analogWrite (11, 0);
+  digitalWrite (8, LOW);
+  digitalWrite (7, HIGH);
+  delay (time);
+  analogWrite (11, speedA);
+  analogWrite (6, speedB);
+}
+
+void right (int time) {
+  analogWrite (11, 150);
+  analogWrite (6, 0);
+  digitalWrite (13, LOW);
+  digitalWrite (12, HIGH);
+  delay (time);
+  analogWrite (11, speedA);
+  analogWrite (6, speedB);
+}
+
+void pause (int time){
+  analogWrite (11, 0);
+  analogWrite (6, 0);
+  delay (time);
+  analogWrite (11, speedA);
+  analogWrite (6, speedB);    
+}
+
+void brake () {
+  analogWrite (11, 0);
+  analogWrite (6, 0);
+}
