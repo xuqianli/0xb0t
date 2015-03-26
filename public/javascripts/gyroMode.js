ed8ba3cc22db549155164413487f9c0a79e2a84d
Steven Li <xq6li@uwaterloo.ca>
2015-03-26 12:16:52 -0400

    gyroMode support left right up and down

--- public/javascripts/gyroMode.js
+++ public/javascripts/gyroMode.js
@@ -31,25 +31,56 @@ $(
           document.getElementById("alpha").innerHTML = alpha;
           document.getElementById("beta").innerHTML = beta;
           document.getElementById("gamma").innerHTML = gamma;
-          if (mode){
-            //forward or backward
-            if (betaLock - beta >= 20) {
-              socket.emit('keydown', 'up');
-            } else if (betaLock - beta <= -20) {
-              socket.emit('keydown', 'down');
-            } else if (betaLock - beta < 20 && betaLock - beta > -20){
-              socket.emit('keyup');
-            }
-            // } else if (gammaLock - gamma >= 20) {
-            //   socket.emit('keydown', 'left');
-            // } else if (gammaLock - gamma <= -20) {
-            //   socket.emit('keydown', 'right');
-            // }
-            // else if (gammaLock - gamma <20 && gammaLock - gamma > -20){
-            //   socket.emit('keyup');            
-            // }
+          var gammaCheck = (gammaLock - gamma <20) && (gammaLock - gamma > -20);
+          var betaCheck = (betaLock - beta < 20) && (betaLock - beta > -20);
 
-          }
+          if (mode) {
+
+            if (gammaCheck && betaCheck) { // if both gamma and betta are in the stop zone then stop
+              socket.emit('keyup'); 
+            } else {
+
+              if (gammaCheck) {
+
+                if (betaLock - beta >= 20){
+                  socket.emit('keydown', 'up');
+                } else if (betaLock - beta <= -20) {
+                  socket.emit('keydown', 'down');
+                }
+
+              } else if (betaCheck) {
+
+                if (gammaLock - gamma >= 20) {
+                  socket.emit('keydown', 'left');
+                } else if (gammaLock - gamma <= -20) {
+                  socket.emit('keydown', 'right');
+                }
+                
+              }
+
+
+            }  
+          }          
+
+          // if (mode){
+          //   //forward or backward
+          //   if (betaLock - beta >= 20 && ) {
+          //     socket.emit('keydown', 'up');
+          //   } else if (betaLock - beta <= -20) {
+          //     socket.emit('keydown', 'down');
+          //   } else if (betaLock - beta < 20 && betaLock - beta > -20){
+          //     socket.emit('keyup');
+          //   }
+          //   // } else if (gammaLock - gamma >= 20) {
+          //   //   socket.emit('keydown', 'left');
+          //   // } else if (gammaLock - gamma <= -20) {
+          //   //   socket.emit('keydown', 'right');
+          //   // }
+          //   // else if (gammaLock - gamma <20 && gammaLock - gamma > -20){
+          //   //   socket.emit('keyup');            
+          //   // }
+
+          // }
 
     },100);    
 
